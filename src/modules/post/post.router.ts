878f0f20427ec6682971d358a2519fa92e8ac528
Alif Ahmed <alifahmed102@gmail.com>
2026-02-02 18:10:18 +0600

    create post

---
+++ src/modules/post/post.router.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { PostController } from './post.controller';
+
+const router = express.Router();
+
+
+router.post(
+    "/",
+    PostController.createPost
+)
+
+
+router.get(
+    "/",
+    PostController.getAllPosts
+);
+router.patch("/:id", PostController.updatePost);
+router.delete("/:id", PostController.deletePost);
+
+
+export const postRouter = router;
